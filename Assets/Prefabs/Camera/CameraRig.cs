82d50e9973276cfe4f277183e937c0d3a6547ace
Li <jili1@uiwtx.edu>
2023-09-14 14:38:16 -0500

    camera follow

---
+++ Assets/Prefabs/Camera/CameraRig.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteAlways]
+public class CameraRig : MonoBehaviour
+{
+    [SerializeField] Transform followTransform;
+    [SerializeField] float armLength;
+    [SerializeField] Transform cameraTrans;
+    [SerializeField] Transform cameraArm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        cameraTrans.position = cameraArm.position - cameraTrans.forward * armLength; 
+        transform.position = followTransform.position;
+    }
+}
+
