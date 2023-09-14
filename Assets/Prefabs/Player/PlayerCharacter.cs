b9d008efa0708cffefd3b1ce6577b9f224d8a774
Li <jili1@uiwtx.edu>
2023-09-14 13:59:24 -0500

    refactored input

--- Assets/Prefabs/Player/PlayerCharacter.cs
+++ Assets/Prefabs/Player/PlayerCharacter.cs
@@ -9,6 +9,9 @@ public class PlayerCharacter : MonoBehaviour
     [SerializeField] private Joystick aimStick;
     [SerializeField] float moveSpeed = 5f;
     CharacterController characterController;
+    Vector2 moveInput;
+    Vector2 aimInput;
+
     private void Awake()
     {
         moveStick.onInputValueChanged += MoveInputUpdated;
@@ -19,12 +22,12 @@ public class PlayerCharacter : MonoBehaviour
 
     private void AimInputUpdated(Vector2 inputVal)
     {
-
+        aimInput = inputVal;
     }
 
     private void MoveInputUpdated(Vector2 inputVal)
     {
-        characterController.Move(new Vector3(inputVal.x, 0f, inputVal.y) * Time.deltaTime * moveSpeed);
+        moveInput = inputVal; 
     }
 
     // Start is called before the first frame update
@@ -36,6 +39,11 @@ public class PlayerCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessMoveInput();
+    }
+
+    private void ProcessMoveInput()
+    {
+        characterController.Move(new Vector3(moveInput.x, 0f, moveInput.y) * moveSpeed * Time.deltaTime);
     }
 }
